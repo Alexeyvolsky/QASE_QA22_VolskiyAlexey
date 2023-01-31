b5a8fe8fd523289a6c13afc6ce7d4feb70de3074
Alexeyvolsky <aleksvolsky@gmail.com>
2023-01-31 18:11:49 +0300

    Selenide

--- src/test/java/SelenideTest.java
+++ src/test/java/SelenideTest.java
@@ -3,6 +3,7 @@ import com.codeborne.selenide.Selenide;
 import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import static com.codeborne.selenide.Condition.*;
@@ -14,11 +15,13 @@ public class SelenideTest {
     public final static By INPUT_EMAIL = By.id("inputEmail");
     public final static By INPUT_PASSWORD = By.id("inputPassword");
     public final static By LOGIN_BUTTON = By.id("btnLogin");
-    public final static String BASE_URL = "https://app.qase.io/login";
     public final static String EMAIL = "aleksvolsky@gmail.com";
     public final static String PASSWORD = "Alex1862123";
     public final static String DEFECT = "Defect";
-
+    @BeforeMethod
+    public void navigate() {
+        open("https://app.qase.io/login");
+    }
 
     @AfterClass(alwaysRun = true)
     public void tearDown() {
@@ -26,31 +29,45 @@ public class SelenideTest {
     }
     @Test
     public void projectTest(){
-        open(BASE_URL);
         $(INPUT_EMAIL).setValue(EMAIL);
         $(INPUT_PASSWORD).setValue(PASSWORD);
         $(LOGIN_BUTTON).click();
         $("#createButton").shouldBe(visible).click();
         $("#project-name").setValue("QWE");
         $("#project-code").setValue("12345678");
-        $("#description-area").setValue("This is my project");
+        $("#description-area").setValue("This is my first project");
         $(byXpath("//span[text()='Create project']//ancestor::button")).click();
         $(byXpath("//div[@class='na4G5n']/div")).shouldBe(visible);
     }
     @Test
     public void testcaseTest(){
-        projectTest();
+        $(INPUT_EMAIL).setValue(EMAIL);
+        $(INPUT_PASSWORD).setValue(PASSWORD);
+        $(LOGIN_BUTTON).click();
+        $("#createButton").shouldBe(visible).click();
+        $("#project-name").setValue("QWE");
+        $("#project-code").setValue("12345678");
+        $("#description-area").setValue("This is my second project");
+        $(byXpath("//span[text()='Create project']//ancestor::button")).click();
+        $(byXpath("//div[@class='na4G5n']/div")).shouldBe(visible);
         $("#create-case-button").click();
         $("#title").setValue("Authorization");
         $("#save-case").click();
-        $("span.OL6rtE").shouldHave(text("Test case was created successfully!"));
-        sleep(5000);
+        $("#create-case-button").shouldBe(visible);
         Assert.assertTrue($("#create-case-button").isDisplayed());
 
     }
     @Test
     public void defectTest(){
-        projectTest();
+        $(INPUT_EMAIL).setValue(EMAIL);
+        $(INPUT_PASSWORD).setValue(PASSWORD);
+        $(LOGIN_BUTTON).click();
+        $("#createButton").shouldBe(visible).click();
+        $("#project-name").setValue("QWE");
+        $("#project-code").setValue("12345678");
+        $("#description-area").setValue("This is my third project");
+        $(byXpath("//span[text()='Create project']//ancestor::button")).click();
+        $(byXpath("//div[@class='na4G5n']/div")).shouldBe(visible);
         $(byXpath("//span[text()='Defects']//ancestor::a")).click();
         $(byXpath("//a[@class='btn btn-primary' and text()='Create new defect']")).click();
         $("#title").setValue("Regression");
